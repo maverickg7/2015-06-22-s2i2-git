46454b6ae51f67f23427b29dfc1dc39549969b3c
Kyle <tpcksh323@gmail.com>
2015-06-23 13:46:54 -0400

    History before noon 23Jun2015

---
+++ src/git-before-noon.hs
@@ -0,0 +1,500 @@
+  148  ls
+  149  cd gas
+  150  vi ~/.cshrc
+  151  (~/small_tools/Extr_xyz/src/Extr_xyz.o | ~/small_tools/IAD_Calc/src/IAD_Calc.o) < 7.xyz > 7.test
+  152  dist 7.xyz > 7.test
+  153  dist < 7.xyz > 7.test
+  154  vi 7.test
+  155  vi 7.test
+  156  ls
+  157  ls
+  158  oaks
+  159  oakscd MBE
+  160  ls
+  161  oaks
+  162  oaks
+  163  oaks
+  164  oaks
+  165  ssh liuky@ccuf0.life.nctu.edu.tw
+  166  ssh liuky@ccuf0.life.nctu.edu.tw
+  167  ssh liuky@ccuf0.life.nctu.edu.tw
+  168  ls
+  169  ls
+  170  vi ~/.bash_profile 
+  171  vi ~/.bash_profile 
+  172  source ~/.bash_profile
+  173  vi ~/.bash_profile 
+  174  source ~/.bash_profile
+  175  cd Desktop/Lab/MBE
+  176  ls
+  177  mkdir mull
+  178  mkdir tip3p
+  179  cd gas
+  180  ls
+  181  oakf
+  182  ls
+  183  dist < 6.head > 6.test
+  184  ls
+  185  vi ~/.bash_profile 
+  186  vi ~/.bash_profile 
+  187  source ~/.bash_profile
+  188  dist < 6.head > 6.test
+  189  vi 6.test
+  190  dist < 8.head > 8.test
+  191  dist < 10.head > 10.test
+  192  vi 10.test
+  193  vi 8.test
+  194  vi 8.test
+  195  ls
+  196  foreach f (*.head)
+  197  man foreach
+  198  foreach f (*.head)
+  199  man foreach
+  200  echo foreach
+  201  csh
+  202  oaks
+  203  ls
+  204  cd Documents/workplace
+  205  ls
+  206  cd Documents
+  207  ls
+  208  cd workspace/
+  209  ls
+  210  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  211  echo brew
+  212  brew install gnuplot
+  213  brew install gnuplot
+  214  cd /usr/bin
+  215  ls
+  216  ls ruby
+  217  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  218  brew install gnuplot
+  219  brew postinstall fontconfig
+  220  ls
+  221  gnuplot
+  222  gnuplot
+  223  ls
+  224  cd Desktop/Lab/MBE/gas
+  225  oakf
+  226  ls
+  227  gnuplot
+  228  brew uninstall gnuplot;brew install gnuplot --with-x
+  229  gnuplot
+  230  cd ..
+  231  cp gas/*.gas ./
+  232  cp chelpg/*.chelpg ./
+  233  cp mull/*.mull ./
+  234  cp tip3p/*.tip3p ./
+  235  ls
+  236  rm MBE.zip 
+  237  ls
+  238  gnuplot
+  239  vi ~/.bash_profile 
+  240  gnuplot
+  241  gnuplot
+  242  ls
+  243  cd chelpg/
+  244  ls
+  245  ls
+  246  ls *.deij
+  247  :q
+  248  vi ~/.bash_profile 
+  249  vi ~/.bash_profile 
+  250  gnuplot
+  251  cd ..
+  252  set y2tics border
+  253  ls
+  254  gnuplot
+  255  vi ~/.bash_profile 
+  256  gnuplot
+  257  gnuplot
+  258  gnuplot
+  259  cp 6-47.gas random-points
+  260  set boxwidth 0.05 absolute
+  261  set xzeroaxis linetype 0 linewidth 1.000
+  262  set yzeroaxis linetype 0 linewidth 1.000
+  263  set zzeroaxis linetype 0 linewidth 1.000
+  264  set title "Uniform Distribution" 
+  265  bin(x, s) = s*int(x/s)
+  266  GPFUN_bin = "bin(x, s) = s*int(x/s)"
+  267  gnuplot
+  268  ls
+  269  plot "random-points" u 1:(0.25*rand(0)-.35)
+  270  gnuplot
+  271  vi ~/.bashrc
+  272  vi ~/.bash_profile 
+  273  gnuplot
+  274  vi ~/.bash_profile 
+  275  vi ~/.bash_profile 
+  276  ls
+  277  gnuplot
+  278  gnuplot
+  279  vi ~/.bash_profile 
+  280  gnuplot
+  281  vi ~/.bash_profile 
+  282  gnuplot
+  283  vi ~/.bash_profile 
+  284  vi ~/.bash_profile 
+  285  gnuplot
+  286  vi ~/.bash_profile 
+  287  vi ~/.bash_profile 
+  288  gnuplot
+  289  ls
+  290  gnuplot
+  291  vi ~/.bash_profile 
+  292  vi ~/.bash_profile 
+  293  gnuplot
+  294  vi ~/.bash_profile 
+  295  vi ~/.bash_profile 
+  296  gnuplot
+  297  ls
+  298  gnuplot
+  299  vi ~/.bash_profile 
+  300  gnuplot
+  301  oaks
+  302  oaks
+  303  ls
+  304  cd Documents/workspace/3B_dist/
+  305  cd src/
+  306  ls
+  307  gcc 3B_dis.c -o 3B_dis.o
+  308  ls
+  309  cp ~/Desktop/Lab/MBE/gas/10.xyz ./
+  310  ls
+  311  vi 10.xyz
+  312  xyz < 10.xyz > 10.abc
+  313  ls
+  314  cd ~/Desktop/Lab/MBE/gas/
+  315  ls
+  316  lls
+  317  vi 6.xyz
+  318  cd ~/Documents/workspace/3B_dist/src/
+  319  ls
+  320  ~/Documents/workspace/Extr_xyz/src/Extr_xyz.o < 10.xyz > 10.abc
+  321  ls
+  322  10.abc
+  323  vi 10.abc
+  324  ./3B_dis.o < 10.abc > 10.3dist
+  325  vi 10.3dist 
+  326  ls
+  327  cp ~/Desktop/Lab/MBE/gas/6.out ./
+  328  ls
+  329  mv 6.out ../../3B_deij/src/
+  330  cd ../../3B_deij/src/
+  331  ls
+  332  vi ~/.bash_profile 
+  333  ls
+  334  grep Fragment 6.out
+  335  :q
+  336  grep 6.out > 6.en
+  337  grep Fragment 6.out > 6.en
+  338  ls
+  339  vi 6.en
+  340  chsh -s /bin/csh
+  341  ls
+  342  vi ~/.cshrc
+  343  mono < 6.out > 6.mono
+  344  source ~/.cshrc
+  345  ls
+  346  pwd
+  347  echo $SHELL
+  348  chsh -s /bin/csh
+  349  chsh -c /bin/csh
+  350  chsh /bin/csh
+  351  chsh -s  /bin/csh
+  352  pwd
+  353  echo mono
+  354  mono < 6.out > 6.mono
+  355  chsh -s  /bin/csh
+  356  echo $BASH_VERSION
+  357  ls
+  358  echo $SHELL
+  359  which nano
+  360  nano
+  361  rubys
+  362  rubys
+  363  ssh osu8115@ruby.osc.edu
+  364  ls
+  365  rubys
+  366  osu8115@ruby.osu.edu
+  367  ssh osu8115@ruby.osu.edu
+  368  ssh osu8115@ruby.osc.edu
+  369  ssh osu8115@ruby.osc.edu
+  370  echo $SHELL
+  371  ls
+  372  whoami
+  373  man ls
+  374  man cd
+  375  man ls
+  376  man cd
+  377  cd Desktop/
+  378  ls
+  379  cd summer_school/
+  380  ls
+  381  man rm
+  382  nano file1.txt
+  383  ls
+  384  git init
+  385  ls
+  386  cd ..
+  387  ls
+  388  mv summer_school/swc_sss
+  389  cd summer_school/
+  390  ls
+  391  git init
+  392  cd ..
+  393  ls
+  394  mv summer_school/ swc_sss
+  395  cd swc_sss/
+  396  ls
+  397  ls shell-novice-gh-pages/
+  398  cd shell-novice-gh-pages/data/Users/nelle/data/pdb/
+  399  ls
+  400  vi glycol.pdb 
+  401  pwd
+  402  wc glycol.pdb 
+  403  wc -l *.pdb 
+  404  wc -l *.pdb >lengths.txt
+  405  sort -n lengths.txt > lengths-sorted.txt
+  406  vi *sort
+  407  vi lengths-sorted.txt
+  408  history
+  409  head lengths-sorted.txt | tail -n 1
+  410  wc -l *pdb | sort -n | head | tail -n l
+  411  echo 'wc -l *pdb | sort -n | head | tail -n l'
+  412  echo 'wc -l *pdb | sort -n | head | tail -n l' > middle.sh
+  413  bash middle.sh
+  414  echo 'wc -l *pdb | sort -n | head | tail -n 1' > middle.sh
+  415  bash middle.sh 
+  416  nano middle.sh 
+  417  ls /bin
+  418  vi middle.sh 
+  419  vi middle.sh 
+  420  vi middle.sh 
+  421  bash 1 middle.sh 
+  422  bash -1 middle.sh 
+  423  bash middle.sh 1
+  424  bash middle.sh 3
+  425  vi middle.sh 
+  426  bash middle.sh 3 10
+  427  bash middle.sh 10 3
+  428  bash middle.sh 3 20
+  429  vi middle.sh 
+  430  cd ~/Desktop/
+  431  mkdir folder2_loops
+  432  cd folder2_loops/
+  433  ls
+  434  touch file{1..10}.txt
+  435  ls
+  436  ls
+  437  vi rename.sh
+  438  bash rename.sh 
+  439  vi rename.sh 
+  440  bash rename.sh 
+  441  ls
+  442  vi rename.sh 
+  443  bash rename.sh 
+  444  ls
+  445  vi rename.sh 
+  446  ls
+  447  vi rename.sh 
+  448  bash rename.sh 
+  449  vi rename.sh 
+  450  bash rename.sh 
+  451  ls
+  452  cd ~/Desktop/swc_sss/shell-novice-gh-pages/data/Users/nelle/writing/
+  453  ls
+  454  grep not ./haiku.txt 
+  455  grep day -n ./haiku.txt 
+  456  grep day -n -i ./haiku.txt 
+  457  man grep
+  458  find . 
+  459  man find
+  460  set b=5
+  461  echo %b
+  462  echo $b
+  463  set a=5
+  464  echo $a
+  465  ssh hpc_11@blueridge2.arc.vt.edu
+  466  ssh hpc_11@blueridge2.arc.vt.edu
+  467  ssh hpc11@blueridge2.arc.vt.edu
+  468  source ~/.bash_profile
+  469  source ~/.bash_profile
+  470  vi ~/.bash_profile
+  471  source ~/.bash_profile
+  472  exit
+  473  vi ~/.bash_profile
+  474  vi ~/.bash_profile
+  475  ssh hpc11@blueridge2.arc.vt.edu
+  476  source ~/.bash_profile
+  477  hpc
+  478  ssh ous8115@ruby.osc.edu
+  479  ssh ous8115@ruby.osc.edu
+  480  ssh ous8115@ruby.osc.edu
+  481  ssh osu8115@ruby.osc.edu
+  482  ls
+  483  ll
+  484  ipython
+  485  ipython
+  486  which ipython
+  487  pip install ipyton
+  488  ipython
+  489  which python
+  490  python
+  491  cd Desktop/swc_sss/
+  492  ls
+  493  cd yob2013.txt
+  494  vi yob2013.txt
+  495  cd Desktop/swc_sss/
+  496  vi babynames_0.py
+  497  vi babynames_0.py
+  498  vim babynames_0.py 
+  499  vim babynames_0.py 
+  500  exit
+  501  ssh osu8115@ruby.osc.edu
+  502  clear
+  503  cd Desktop/
+  504  ls
+  505  cd swc_sss/
+  506  ls
+  507  cd ..
+  508  ls
+  509  mkdir git
+  510  cd git
+  511  mkdir 2015-06-22-s2i2
+  512  mv 2015-06-22-s2i2/ 2015-06-22-s2i2-git
+  513  cd 2015-06-22-s2i2-git/
+  514  ls
+  515  mkdir src
+  516  vi README.md
+  517  vi README.md 
+  518  which git
+  519  ls
+  520  ls -a
+  521  ls
+  522  git init
+  523  ls
+  524  ls -a
+  525  git status
+  526  git config --global user.name "Kyle"
+  527  git config --global user.email "tpcksh323@gmail.com"
+  528  git config --global color.ui "auto"
+  529  git status
+  530  git config --list
+  531  git status
+  532  git add README.md 
+  533  git status
+  534  git commit
+  535  git status
+  536  cd src/
+  537  ls
+  538  touch .gitkeep
+  539  git status
+  540  git add .
+  541  git status
+  542  git commit -m "addd gitkeep file in src folder"
+  543  vi .gitkeep 
+  544  git status
+  545  vi .gitkeep
+  546  ls
+  547  cd ..
+  548  ls
+  549  vi README.md 
+  550  cd src/
+  551  ls
+  552  ls -a
+  553  vi ../.git
+  554  vi tmp.py
+  555  mv tmp.py temperature.py
+  556  python temperature.py 
+  557  vi temperature.py 
+  558  python temperature.py 
+  559  vi temperature.py 
+  560  python temperature.py 
+  561  :q
+  562  git status
+  563  git add .
+  564  git status
+  565  git commit -m "add temp f_to_k function"
+  566  git status
+  567  vi temperature.py 
+  568  git status
+  569  git diff
+  570  git add .
+  571  git commit -m 'fixed temp convert in f_to_k'
+  572  git status
+  573  vi temperature.py 
+  574  git status
+  575  git diff
+  576  python temperature.py 
+  577  git add
+  578  git add .
+  579  git status
+  580  git diff
+  581  git diff HEAD
+  582  git commit -m 'all temp convert functions'
+  583  git status
+  584  git log
+  585  git log --oneline
+  586  git status
+  587  git diff HEAD~1 temperature.py 
+  588  git diff HEAD~2 temperature.py 
+  589  git log --oneline
+  590  git diff 47f3497 temperature.py 
+  591  git checkout 84584c4 temperature.py 
+  592  git status
+  593  vi temperature.py 
+  594  git log --oneline
+  595  git check out c8fa4aa temperature.py 
+  596  git checkout c8fa4aa temperature.py 
+  597  vi temperature.py 
+  598  git log --oneline
+  599  git status
+  600  git status
+  601  git checkout master
+  602  git log --oneline --graph --all --decorate
+  603  history
+  604  touch something _i_dont_want
+  605  git status
+  606  ls
+  607  rm _i_dont_want
+  608  ls
+  609  mv something something_i_dont_want
+  610  ls
+  611  git status
+  612  pwd
+  613  cd ..
+  614  touch .gitignore
+  615  vi .gitignore
+  616  git status
+  617  git add .gitignore
+  618  git commit -m 'igore some files'
+  619  git status
+  620  ls
+  621  cd src/
+  622  ls
+  623  rm temperature.py 
+  624  ls
+  625  git status
+  626  git reset --hard
+  627  git status
+  628  ls
+  629  vi temperature.py 
+  630  git status
+  631  pwd
+  632  cd ..
+  633  git remote add origin https://github.com/maverickg7/2015-06-22-s2i2-git.git
+  634  git remote -v
+  635  git push  origin master
+  636  ls
+  637  cd src/
+  638  ls
+  639  vi README.md
+  640  git status
+  641  git add README.md 
+  642  git commit -m 'added reame in src folder'
+  643  git status
+  644  git push origin master
+  645  git pull origin master
+  646  vi README.md 
+  647  history | cat > git-before-noon.hs
